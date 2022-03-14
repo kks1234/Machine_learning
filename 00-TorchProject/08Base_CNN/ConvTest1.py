b08b273967e52e35cf67f8e7c01aea09b763b5a3
kks1234 <945366712@qq.com>
2022-03-14 17:32:38 +0800

    conv shape test1

--- 00-TorchProject/08Base_CNN/ConvTest1.py
+++ 00-TorchProject/08Base_CNN/ConvTest1.py
@@ -5,12 +5,12 @@ width, height = 100, 100
 kernel_size = 3
 batch_size = 1
 
-input = torch.randn(batch_size, in_channels, width, height)
+input_data = torch.randn(batch_size, in_channels, width, height)
 
 conv_layer = torch.nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=1, bias=False)
 
-out = conv_layer(input)
+out = conv_layer(input_data)
 
-print(input.shape)
+print(input_data.shape)
 print(out.shape)
 print(conv_layer.weight.shape)
